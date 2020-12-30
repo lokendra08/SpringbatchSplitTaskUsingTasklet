962749ca37a3cabaaccb200093f0ee5884dcd007
techcreation85 <60753576+techcreation85@users.noreply.github.com>
2020-12-30 12:53:22 +0530

    first commit

--- src/main/java/com/batch/demo/config/BatchConfiguration.java
+++ src/main/java/com/batch/demo/config/BatchConfiguration.java
@@ -5,10 +5,15 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 
 import com.batch.demo.listeners.JobListener;
 import com.batch.demo.listeners.TaskThreeListener;
@@ -55,10 +60,45 @@ public class BatchConfiguration {
 	     return jobs.get("LokiJob")
 	                .incrementer(new RunIdIncrementer())
 	                .listener(new JobListener())
-	                .start(stepOne())
-	                .next(stepTwo())
-	                .next(stepThree())
-	                .build();
-	}            
+	                .start(splitFlow())
+	                .build().build();
+	}  
+	
+	@Bean
+	public Flow splitFlow() {
+	  return new FlowBuilder<SimpleFlow>("splitFlow")
+	    .split(taskExecutor())
+	    .add(flow1(),flow3())
+	    .next(flow2())
+	    .build();
+	}
+
+	@Bean
+	public Flow flow1() {
+	  return new FlowBuilder<SimpleFlow>("flow1")
+	    .start(stepOne())
+	    .build();
+	}
+
+	@Bean
+	public Flow flow2() {
+	  return new FlowBuilder<SimpleFlow>("flow2")
+	    .start(stepTwo())
+	    .build();
+	}
+	
+	@Bean
+	public Flow flow3() {
+	  return new FlowBuilder<SimpleFlow>("flow3")
+	    .start(stepThree())
+	    .build();
+	}
+
+	@Bean
+	public TaskExecutor taskExecutor(){
+	  return new SimpleAsyncTaskExecutor("spring_batch_loki");
+	}
+	
+	
 
 }
